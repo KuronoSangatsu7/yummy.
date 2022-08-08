31b43ff33220f83b5df9c6787a48aac40e1e4972
Jaffar <jacepac264@gmail.com>
2022-08-08 11:23:42 +0300

    initialized react app and tailwind

---
+++ src/components/UI/FullWrapper.js
@@ -0,0 +1,11 @@
+const FullWrapper = props => {
+  return (
+    <div
+      className="h-full overflow-scroll bg-beige"
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default FullWrapper;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        "beige": "#EEEAED",
+        "roseRed": "#FE0435",
+        "kindaBlack": "#181E27",
+        "darkGreen": "#00272B",
+      },
+    },
+  },
+  plugins: [],
+};

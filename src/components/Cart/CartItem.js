24c09556aa1927e06fbdd19d3a9b9cd25c0685a8
Jaffar <jacepac264@gmail.com>
2022-08-09 16:37:19 +0300

    items can now be added directly from cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,20 +1,26 @@
+import { useCart } from "../../store/CartContextProvider";
 import InvertedMiniButton from "../UI/Buttons/InvertedMiniButton";
 import MiniButton from "../UI/Buttons/MiniButton";
 
 const CartItem = (props) => {
-  const price = `$${props.price.toFixed(2)}`
+  const {addItem, removeItem} = useCart();
+  const price = `$${props.item.price.toFixed(2)}`
+
+  const addItemHandler = (item) => {
+    addItem({...item, amount: 1})
+  }
 
   return (
     <div className="flex-col space-y-3 snap-start">
-      <div className="text-xl">{props.name}</div>
+      <div className="text-xl">{props.item.name}</div>
       <div className="flex justify-between">
         <div className="flex space-x-4">
           <div className="font-semibold">{price}</div>
-          <div>x{props.amount}</div>
+          <div>x{props.item.amount}</div>
         </div>
         <div className="grid-rows space-x-2">
           <InvertedMiniButton className="w-10 grow-0">-</InvertedMiniButton>
-          <MiniButton className="w-10">+</MiniButton>
+          <MiniButton className="w-10" onClick={addItemHandler.bind(null, props.item)}>+</MiniButton>
         </div>
       </div>
     </div>
--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -1,8 +1,8 @@
 import CartItem from "./CartItem"
 
 const CartList = props => {
-    const itemList = props.items.map((item) => <CartItem name={item.name} amount={item.amount} price={item.price} key={item.id}/>)
-    return <div className="flex-col space-y-4 max-h-80 overflow-auto scroll-smooth snap-y scroll-pb-12">{itemList}</div>
+    const itemList = props.items.map((item) => <CartItem item={{...item}} key={item.id}/>)
+    return <div className="flex-col space-y-4 max-h-64 p-4 overflow-auto scroll-smooth snap-y scroll-pb-12">{itemList}</div>
 }
 
 export default CartList

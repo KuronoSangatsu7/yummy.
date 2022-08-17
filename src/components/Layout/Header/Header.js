39d9d7f4cbd52c4ba55995af359a6fa276680666
Jaffar <jacepac264@gmail.com>
2022-08-17 09:32:17 +0300

    added clear cart button

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,7 +1,10 @@
+import { useCart } from "../../../store/CartContextProvider";
 import Button from "../../UI/Buttons/Button";
 import ShoppingCartButton from "./ShoppingCartButton/ShoppingCartButton";
 
 const Header = (props) => {
+  const {clearCart} = useCart()
+
   return (
     <nav className="relative container mx-auto p-6 flex items-center justify-between">
       <div className="text-roseRed text-2xl font-medium subpixel-antialiased tracking-wider">
@@ -12,7 +15,7 @@ const Header = (props) => {
       </div>
       <div className="flex justify-between items-center space-x-3">
         <ShoppingCartButton onClick={props.onShowCart}/>
-        <Button className="px-6">Confirm Order</Button>
+        <Button className="px-6" onClick={clearCart}>Clear Cart</Button>
       </div>
     </nav>
   );
